53c890ce062124959d3e9ac2e9199571ad33bb09
Henry23411 <65517936+Henry23411@users.noreply.github.com>
2020-06-16 01:26:00 -0400

    now accepts dividend yield in calculation

--- options_pricing_and_visualizer/src/com/uoft/henry/javatest/options_calculations.java
+++ options_pricing_and_visualizer/src/com/uoft/henry/javatest/options_calculations.java
@@ -5,36 +5,38 @@ import java.math.RoundingMode;
 
 public class options_calculations {
 	
-	public static double call_calc (double S, double K, double t, double r, double stdev) {
-		double a = Math.log(S/K); // ln(S0/K)
+	public static double call_calc (double S, double X, double t, double q,double r, double stdev) {
+		double a = Math.log(S/X); // ln(S0/K)
 		double b = ((r + ((stdev * stdev) / 2)) * t); // (r+σ2/2)t
 		double c = stdev * Math.sqrt(t); // σ√t
 		double d1 = (a+b)/c;
 		double d2 = d1 - c;
 		double nd1 = normdist.getAreaUnderNormalCurve(-5, d1);
 		double nd2 = normdist.getAreaUnderNormalCurve(-5, d2);
-		double e = Math.exp(-r * t);
+		double e1 = Math.exp(-q * t);
+		double e2 = Math.exp(-r * t);
 		
-		double price = (nd1 * S) - (nd2 * K * e);
+		double price = (S * e1 * nd1) - (X * e2 * nd2);
 		
 		BigDecimal bd = new BigDecimal(price).setScale(2, RoundingMode.HALF_UP);
-        return price = bd.doubleValue();
+        return price = bd.doubleValue(); // rounding price to hearest hundredth
 		
 	}
 	
-	public static double put_calc (double S, double K, double t, double r, double stdev) {
-		double a = Math.log(S/K); // ln(S0/K)
+	public static double put_calc (double S, double X, double t, double q, double r, double stdev) {
+		double a = Math.log(S/X); // ln(S0/K)
 		double b = ((r + ((stdev * stdev) / 2)) * t); // (r+σ2/2)t
 		double c = stdev * Math.sqrt(t); // σ√t
 		double d1 = (a+b)/c;
 		double d2 = d1 - c;
 		double nd1 = normdist.getAreaUnderNormalCurve(-5, -d1);
 		double nd2 = normdist.getAreaUnderNormalCurve(-5, -d2);
-		double e = Math.exp(-r * t);
+		double e1 = Math.exp(-r * t);
+		double e2 = Math.exp(-q * t);
 		
-		double price = (K * e * nd2) - (S * nd1);
+		double price = (X * e1 * nd2) - (S * e2 * nd1);
 		
 		BigDecimal bd = new BigDecimal(price).setScale(2, RoundingMode.HALF_UP);
-        return price = bd.doubleValue();
+        return price = bd.doubleValue(); // rounding price to hearest hundredth
 	}
 }
--- options_pricing_and_visualizer/src/com/uoft/henry/javatest/options_pricing.java
+++ options_pricing_and_visualizer/src/com/uoft/henry/javatest/options_pricing.java
@@ -4,34 +4,37 @@ import java.util.Scanner;
 public class options_pricing {
 	
 	public static void main(String args[]) {
-		double S, K, t, r, stdev;
+		double S, X, t, q, r, stdev;
 		try (Scanner scan = new Scanner (System.in)) {
 			System.out.print("Enter option type (call/put): ");
 			String option = scan.nextLine();
 			option = option.toLowerCase();
 			
-			System.out.print("Stock Price(S): ");
+			System.out.print("Underlying Price: ");
 			S = scan.nextDouble();
 			
-			System.out.print("Exercise Price(K): ");
-			K = scan.nextDouble();
+			System.out.print("Strike Price: ");
+			X = scan.nextDouble();
 			
-			System.out.print("Time to maturity in years (t): ");
+			System.out.print("Time to exprations (in years): ");
 			t = scan.nextDouble();
 			
-			System.out.print("Annual Risk Free Rate (r): ");
+			System.out.print("Dividend Yield (compounded continuously): ");
+			q = scan.nextDouble();
+			
+			System.out.print("Annual Risk Free Rate: ");
 			r = scan.nextDouble();
 			
-			System.out.print("Annualized Volatility (σ): ");
+			System.out.print("Annualized Volatility: ");
 			stdev = scan.nextDouble();
 			
 			if (option.equals ("call")) {
-				double call_price = options_calculations.call_calc (S, K, t, r, stdev);
+				double call_price = options_calculations.call_calc (S, X, t, q, r, stdev);
 				System.out.print("Option Price is: " + call_price);	
 			}
 			
 			else if (option.equals("put")) {
-				double put_price = options_calculations.put_calc (S, K, t, r, stdev);
+				double put_price = options_calculations.put_calc (S, X, t, q, r, stdev);
 				System.out.print("Option Price is: " + put_price);
 			}
 
